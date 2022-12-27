a32b02bec4181d9b8e6dabe35b60c89d2b2eabf5
kaibertelsen <kai@expofilm.no>
2022-12-27 08:42:41 +0100

    flere funksjoner

--- autosave-v002.js
+++ autosave-v002.js
@@ -1,17 +1,26 @@
 //Autosave funksjon for webflow
+// disse variablene må settes Inside headertag
+/*
+var autosavefield = [];    //array for navne på felter //keys
+var autosavevalue = [];    //array for verdien i felter //value
+let typingTimer;                //timer identifier
+let doneTypingInterval = 3000;  //tiden det tar fra en slutter å skrive til autosave blir aktivert
+let collectionId = "collectionId from cms";
+let itemId = "itemId from cms";
+*/
 
-//tømmer array ave oppstart og etter mottatt response
+//tømmer array save oppstart og etter mottatt response
 arrayready();
 
+//tømmer array og setter navn som er obligatorisk
 function arrayready(){
 autosavefield = [];
 autosavevalue = [];
 //setter leverandørnavn-nr som opprinnelig
-autosavefield.push("name");
-autosavevalue.push("{{wf {&quot;path&quot;:&quot;name&quot;,&quot;type&quot;:&quot;PlainText&quot;\} }}");
+setnamevalue();
 }
 
-//input from textarea trigger når en er ferdig å skrive og trykker enter evt. utenfor feltet.
+//Trigger når en er ferdig å skrive og trykker enter evt. utenfor feltet.
 $('textarea'). change(function(){
 if (this.parentElement.classList.contains("autosave")){
 	if(!autosavefield.includes(this.name)){
@@ -27,6 +36,7 @@ if (this.parentElement.classList.contains("autosave")){
 	startcounter();
 });
 
+//Trigger når et hidden field forandrer innhold
 $('input[type=hidden]').change(function(){
 if (this.parentElement.classList.contains("autosavetext")){
 	if(!autosavefield.includes(this.name)){
@@ -42,6 +52,7 @@ if (this.parentElement.classList.contains("autosavetext")){
 	startcounter();
 });
 
+//Trigger når en radiobox endres
 $('input[type=radio]').change(function(){
 if (this.parentElement.classList.contains("autosave")){
 	if(!autosavefield.includes(this.name)){
@@ -57,6 +68,7 @@ if (this.parentElement.classList.contains("autosave")){
 	startcounter();
 });
 
+//Trigger når en select box endres
 $('select'). change(function(){
 if (this.classList.contains("autosave")){
 	if(!autosavefield.includes(this.name)){
@@ -72,6 +84,7 @@ if (this.classList.contains("autosave")){
 	startcounter();
 });
 
+//Trigger når en taster inn i et textfelt
 $('input'). keyup(function(){
 	if (this.classList.contains("autosave")){
 	if(!autosavefield.includes(this.name)){
@@ -86,7 +99,8 @@ $('input'). keyup(function(){
 	}
 	startcounter();
 });
-//innkommende array
+
+//her kan en sende array til et felt
 function arrayautosave(fieldname,array){
 if(!autosavefield.includes(fieldname)){
 	//om den ikke inneholder det tidligere
@@ -100,6 +114,7 @@ if(!autosavefield.includes(fieldname)){
   startcounter();
 }
 
+// Trigges fra en chechbox 
 function boxselect(elementid){
 const element = document.getElementById(elementid);
 	if (element.parentElement.parentElement.classList.contains("autosave")){
@@ -124,8 +139,8 @@ const element = document.getElementById(elementid);
   
 }
 
-function startcounter(){
 //starte counter 
+function startcounter(){
     clearTimeout(typingTimer);
     if (autosavefield.length>0){
     typingTimer = setTimeout(counterdone, doneTypingInterval);
@@ -133,22 +148,36 @@ function startcounter(){
     console.log(autosavefield,autosavevalue);
   }
 
+  //Kjøres når counter er ferdig
 function counterdone() {
-    //autosave
     console.log("Autosave:/n","field:",autosavefield,"value:",autosavevalue);
 
+	//kallet på api funksjonen 
     callapi(collectionId,itemId,autosavefield,autosavevalue,"PATCH","webflow","239");
+	//synliggjør elementer
     document.getElementById("autosave").style.display="Block"
     document.getElementById("autosavedone").style.display="none"
 }
 
 function setsave(){
+	//synliggjør elementer
 	document.getElementById("autosave").style.display="none"
-  document.getElementById("autosavedone").style.display="Block"
-  d = new Date();
+    document.getElementById("autosavedone").style.display="Block"
+    //setter tidspunkt for lagring
+	d = new Date();
 	datetext = d.toTimeString();
 	datetext = datetext.split(' ')[0];
 	document.getElementById("autosavetime").innerHTML = datetext;
 }
 
+
+//funksjonene som trigges etter retur fra api
+function apireturn(data,id){
+	//synligjør elementer
+	setsave();
+	// tømmer array klar for neste autosave
+    arrayready();
+    returdata(data,id)
+
+}
 
