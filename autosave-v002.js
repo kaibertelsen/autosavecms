ffe2df1175ff538997df57fdb85abeed0f547f87
kaibertelsen <117456051+kaibertelsen@users.noreply.github.com>
2022-12-27 08:03:49 +0100

    Update and rename autosave-v001.js to autosave-v002.js

--- autosave-v002.js
+++ autosave-v002.js
@@ -11,6 +11,8 @@ autosavefield.push("name");
 autosavevalue.push("{{wf {&quot;path&quot;:&quot;name&quot;,&quot;type&quot;:&quot;PlainText&quot;\} }}");
 }
 
+/////////input to autosave
+
 $('textarea'). change(function(){
 if (this.parentElement.classList.contains("autosave")){
 	if(!autosavefield.includes(this.name)){
@@ -85,7 +87,52 @@ $('input'). keyup(function(){
 	}
 	startcounter();
 });
+//innkommende array
+function arrayautosave(fieldname,array){
+if(!autosavefield.includes(fieldname)){
+	//om den ikke inneholder det tidligere
+	autosavefield.push(fieldname);
+	autosavevalue.push(array);
+	}else{
+	//oppdatere value i riktig index
+	var index = autosavefield.indexOf(fieldname);
+	autosavevalue[index] = array;
+	}
+  startcounter();
+}
 
+function boxselect(elementid){
+const element = document.getElementById(elementid);
+	if (element.parentElement.parentElement.classList.contains("autosave")){
+     	let value = "false";
+     
+        if (element.checked ==true){
+	value = "true";
+        }
+     
+	if(!autosavefield.includes(element.name)){
+      //om den ikke inneholder det tidligere
+	autosavevalue.push(value);
+	autosavefield.push(element.name);
+	}else{
+      //oppdatere value i riktig index
+	var index = autosavefield.indexOf(element.name);
+	autosavevalue[index] = value;
+      }
+      
+	startcounter();
+  	}
+  
+}
+
+function startcounter(){
+//starte counter 
+    clearTimeout(typingTimer);
+    if (autosavefield.length>0){
+    typingTimer = setTimeout(counterdone, doneTypingInterval);
+    }
+    console.log(autosavefield,autosavevalue);
+  }
 
 function counterdone() {
     //autosave
