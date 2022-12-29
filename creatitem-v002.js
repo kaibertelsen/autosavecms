a1cbfec3157c60444f26684e75225359a62085ee
kaibertelsen <117456051+kaibertelsen@users.noreply.github.com>
2022-12-29 09:45:48 +0100

    oppdatering

--- creatitem-v002.js
+++ creatitem-v002.js
@@ -1,6 +1,6 @@
-function loadarrays (){
+function loadarrays (classname){
 // finne alle elementer med create class
-var els = document.getElementsByClassName("create");
+var els = document.getElementsByClassName(classname);
 for(var i = 0; i < els.length; i++)
 {
   console.log(els[i],"Value:",els[i].value);
